6269ef72508544494f1f788ddadba00857c7e926
sev <meyer483@uwm.edu>
2025-12-24 23:37:50 -0600

    beginning to add functionality to the project

--- samples.py
+++ samples.py
@@ -2,12 +2,13 @@ import sqlite3
 import hashlib
 
 
-
+##connects to the userdata database as initialized below
 connect=sqlite3.connect("userdata.db")
 
 cursor=connect.cursor()
 
-
+##this SQL function creates a table of keys,usernames and passwords and gives the usernames and passwords
+##the ability to hold char values and restricts them from having not null values
 cursor.execute("""               
 CREATE TABLE IF NOT EXISTS userdata(
         id INTEGER PRIMARY KEY,
@@ -16,12 +17,27 @@ CREATE TABLE IF NOT EXISTS userdata(
 )             
 """)
 
-
+##the hashlib import gives me access to various hashing tools including the use of the sha256 hash and being able to put 
+##even simple passwords through it 
 username1, password1= "Sam123", hashlib.sha256("sampassword".encode()).hexdigest()
 
 
 print(username1)
 print(password1)
+
+
+while True:
+    username=input("Please enter a username: ")
+    password=input("Please enter a very secure password: ")
+
+    if(username.isalnum() and password.isalnum()):
+        break
+
+
+cursor.execute("INSERT INTO userdata (username, password) VALUES(?,?)", (username,password))
+
+connect.commit()
+
 
 
 
